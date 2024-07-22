f72cdc3a6d8504db41b229c3e83ebd244dd63c3c
ShieTamaoki <shie0041@gmail.com>
2024-07-23 03:06:48 +0900

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <img src={img9209} alt="Geo Mapper" style={{width: '100%', maxWidth: '600px', marginTop: '20px'}} />
       </header>
     </div>
