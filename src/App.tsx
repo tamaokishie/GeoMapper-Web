0299f2cbef04e2ef4a2f69653318a642431e9fa7
ShieTamaoki <shie0041@gmail.com>
2024-07-23 03:03:05 +0900

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import img9209 from './IMG_9209.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <img src={img9209} alt="Geo Mapper" style={{width: '100%', maxWidth: '600px', marginTop: '20px'}} />
       </header>
     </div>
   );
